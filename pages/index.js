d83fdaca68a9e66aa3bde859ce919c0ce41ddc3c
Furuta Yusuke <furuta.yusuke.012@gmail.com>
2021-02-28 13:17:49 +0900

    Show poml text in web page

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,21 @@ import Text from '../components/Text'
 
 export default function Home () {
   return (
-    <scene>
-      <primitive shape="cube" id="fix_world_position" scale="0.2 0.2 0.2">
-        <script src="./wasm/fix_transform.wasm" args="--position 0.3 0.3 0.1 --world" />
-      </primitive>
-      <primitive shape="cube" id="fix_local_position" scale="0.2 0.2 0.2">
-        <script src="./wasm/fix_transform.wasm" args="--position 0.3 0.6 0.1 --local" />
-      </primitive>
-      <primitive shape="sphere" position="1 0.1 1" scale="0.1 0.1 0.3" />
-      <model src="./gun/gun.glb" position="1 0 0.1" />
-      <Text text="aa"/>
-    </scene>
+    <poml>
+      <scene>
+        <primitive shape="cube" id="fix_world_position" scale="0.2 0.2 0.2">
+          <script src="./wasm/fix_transform.wasm" args="--position 0.3 0.3 0.1 --world" />
+        </primitive>
+
+        <primitive shape="cube" id="fix_local_position" scale="0.2 0.2 0.2">
+          <script src="./wasm/fix_transform.wasm" args="--position 0.3 0.6 0.1 --local" />
+        </primitive>
+        <primitive shape="sphere" position="1 0.1 1" scale="0.1 0.1 0.3" />
+
+        <model src="./gun/gun.glb" position="1 0 0.1" />
+
+        <Text text="test text" />
+      </scene>
+    </poml>
   )
 }
